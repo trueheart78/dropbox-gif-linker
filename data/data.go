5c307eec350ec6ccd4c4d8f737348e1422b01fac
Josh Mills <josh@trueheart78.com>
2018-05-09 09:04:43 -0400

    data handler go :sparkling_heart:

--- data/data.go
+++ data/data.go
@@ -15,10 +15,27 @@ func NewHandler() Handler {
 }
 
 // Clean cleans the input data
-func (h Handler) Clean(data string) string {
-	return fmt.Sprintf("Cleaning [%v]", data)
+func (h Handler) Clean(data string) (clean string, err error) {
+	clean = strings.TrimSpace(data)
+	clean = strings.Replace(clean, "\\", "", -1)
+	if h.hasApostrophes(clean) || h.hasQuotes(clean) {
+		clean = clean[1 : len(clean)-1]
+	}
+	if !h.isGif(clean) {
+		err = fmt.Errorf("not a gif [%v]", clean)
+	}
+	return
 }
 
 func (h Handler) isGif(filename string) bool {
 	return strings.HasSuffix(strings.ToLower(filename), ".gif")
+}
+
+func (h Handler) hasApostrophes(data string) bool {
+	return (strings.HasPrefix(data, "'") && strings.HasSuffix(data, "'"))
+}
+
+func (h Handler) hasQuotes(data string) bool {
+	return (strings.HasPrefix(data, "\"") && strings.HasSuffix(data, "\""))
+
 }
--- data/data_test.go
+++ data/data_test.go
@@ -1,15 +1,32 @@
 package data
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-// TestNil description
-func TestDataIsGif(t *testing.T) {
-	h := NewHandler()
+var h = NewHandler()
 
+func TestDataClean(t *testing.T) {
+	var data string
+	var err error
+
+	for _, d := range dirtyData() {
+		data, err = h.Clean(d)
+
+		assert.Equal(t, "/path/to/file name.gif", data)
+		assert.Nil(t, err)
+	}
+
+	badGif := "/sample/not a gif"
+	_, err = h.Clean(badGif)
+	assert.NotNil(t, err)
+	assert.Equal(t, fmt.Sprintf("not a gif [%v]", badGif), err.Error())
+}
+
+func TestDataIsGif(t *testing.T) {
 	assert.True(t, h.isGif("sample.gif"))
 	assert.True(t, h.isGif("sample.GIF"))
 	assert.False(t, h.isGif("sample.gifk"))
@@ -17,4 +34,29 @@ func TestDataIsGif(t *testing.T) {
 	assert.False(t, h.isGif("sample.if"))
 	assert.False(t, h.isGif("sample.jpg"))
 	assert.False(t, h.isGif("samplegif"))
+}
+
+func TestDataHasApostrophes(t *testing.T) {
+	assert.True(t, h.hasApostrophes("'sample'"))
+	assert.False(t, h.hasApostrophes("sample'"))
+	assert.False(t, h.hasApostrophes("'sample"))
+	assert.False(t, h.hasApostrophes("\"sample\""))
+}
+
+func TestDataHasQuotes(t *testing.T) {
+	assert.True(t, h.hasQuotes("\"sample\""))
+	assert.False(t, h.hasQuotes("sample\""))
+	assert.False(t, h.hasQuotes("\"sample"))
+	assert.False(t, h.hasQuotes("'sample'"))
+}
+
+func dirtyData() []string {
+	data := make([]string, 0)
+	data = append(data, "/path/to/file name.gif")
+	data = append(data, "/path/to/file\\ name.gif")
+	data = append(data, "\"/path/to/file\\ name.gif\"")
+	data = append(data, "'/path/to/file\\ name.gif'")
+	data = append(data, "'/path/to/file\\ name.gif' \n")
+	data = append(data, "\t'/path/to/file\\ name.gif' \n")
+	return data
 }
