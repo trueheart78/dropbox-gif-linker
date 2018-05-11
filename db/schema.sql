388d2f3fa2873e72407611c1c7848c05238fdaa0
Josh Mills <josh@trueheart78.com>
2018-05-11 06:29:22 -0400

    md5 column added to gifs table

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,8 @@
 -- gifs
-CREATE TABLE IF NOT EXISTS "gifs" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "basename" varchar, "directory" varchar, "size" integer, "shared_link_id" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+CREATE TABLE IF NOT EXISTS "gifs" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "basename" varchar, "directory" varchar, "size" integer, "md5" varchar, "shared_link_id" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
 CREATE INDEX IF NOT EXISTS "index_gifs_on_shared_link_id" ON "gifs" ("shared_link_id");
+CREATE INDEX IF NOT EXISTS "index_gifs_on_md5" ON "gifs" ("md5");
+CREATE INDEX IF NOT EXISTS "index_gifs_on_basename_and_directory" ON "gifs" ("basename", "directory");
 
 -- shared_links
 CREATE TABLE IF NOT EXISTS "shared_links" ("id" varchar NOT NULL PRIMARY KEY, "gif_id" integer, "remote_path" varchar, "count" integer DEFAULT 0, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL, CONSTRAINT "fk_golang_35031788c2"
